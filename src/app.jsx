516d4e2e177433ae0bfc05c6bbb253777dca8444
Matheus Moura <matheus.moura13@gmail.com>
2018-10-21 10:40:55 -0300

    Configura react

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <div>Olá eu sou um app</div>
+  }
+}
+
+module.exports = App;
---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './app';
+
+render(<App />, document.getElementById("root"));
+
+// Registering ServiceWorker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function (registration) {
+
+    // Registration was successful
+    console.log('ServiceWorker registration successful. Scope: ' + registration.scope)
+  }).catch(function (err) {
+
+    // Registration failed with error
+    console.log('ServiceWorker registration failed. Error: ' + err);
+  });
+}
