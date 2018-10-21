30051d43ccfd9a9656f8d510461a5cf13ee80dcc
Matheus Moura <matheus.moura13@gmail.com>
2018-10-21 19:15:29 -0300

    Fixa google material

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 
 class App extends React.Component {
   render() {
     return(
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>)
+      <span>ok</span>
+    );
   }
 }
 
